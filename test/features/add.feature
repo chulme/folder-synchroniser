c0acc6cf3e2d1cc776ddf5071b3f60720d5d8307
Chris Hulme <chris_hulme99@outlook.com>
2021-10-24 14:46:58 +0100

    Implemented tests

--- test/features/add.feature
+++ test/features/add.feature
@@ -1,16 +1,19 @@
 Feature: Synchronise by adding new item
     Scenario: Synchronise single tiered directory
-        Given Client and server are initialised
+        Given Client and server are ran concurrently
         When A file is saved in the client directory
         Then The files are synchronised in the server folder
+        Then Concurrent threads are joined
 
     Scenario: Synchronise multi tiered directory
-        Given Client and server are initialised
+        Given Client and server are ran concurrently
         When A file is saved within a folder of the client directory
         Then The files are synchronised in the server folder
+        Then Concurrent threads are joined
 
     Scenario: Synchronise copied file
-        Given Client and server are initialised
+        Given Client and server are ran concurrently
         When A file is saved in the client directory
         When The file is copied and pasted in the client directory
         Then The files are synchronised in the server folder
+        Then Concurrent threads are joined
--- test/steps/add.py
+++ test/steps/add.py
@@ -1,5 +1,6 @@
 from behave import given, when, then
 from pathlib import Path
+import time
 
 import sys
 import os
@@ -11,10 +12,19 @@ import client as cl
 import server as srv
 import filediff
 
-@given(u'Client and server are initialised')
+@given(u'Client and server are ran concurrently')
 def step_imp(context):
-    client = cl.Client()
-    server= srv.Server()
+    client = cl.Client(Path('..\\build\\src'))
+    server= srv.Server(Path('..\\dst\\src'))
+    server.start()
+    client.start()
+    print("Nodes started")
+
+    # Terminate nodes after 5s. To-do, move to seperate then block
+    time.sleep(5)
+    server.terminate()
+    client.terminate()
+
 
 @when(u'A file is saved in the client directory')
 def step_imp(context):
@@ -22,6 +32,8 @@ def step_imp(context):
     Path('..\\build\\dst').mkdir(parents=True, exist_ok=True)
     file = Path('..\\build\\src\\file.txt')
     file.write_bytes(b'Hello World!')
+    print("File saved in client dir")
+
 
 @when(u'A file is saved within a folder of the client directory')
 def step_imp(context):
@@ -30,18 +42,29 @@ def step_imp(context):
     Path('..\\build\\dst').mkdir(parents=True, exist_ok=False)
     file = Path('..\\build\\src\\my_folder\\file.txt')
     file.open("w", encoding = "utf-8").write('Hello World!')
+    print("File saved within folder of client dir")
 
 
 @when(u'The file is copied and pasted in the client directory')
 def step_imp(context):
     shutil.copy(Path('..\\build\\src\\file.txt'), '..\\build\\src\\file2.txt')
+    print("File copied")
 
 
 @then(u'The files are synchronised in the server folder')
 def step_imp(context):
     files = filediff.FileDiff().get_files_in_dir('..\\build\\src')
+    assert len(files)>0
     for file in files:
         dst_equivalent = Path(str(file).replace("src", "dst"))
         assert file.read_bytes() == dst_equivalent.read_bytes()
+    print("Files synced")
+
+
+@then(u'Concurrent threads are joined')
+def step_imp(context):
+    print("Not implemented")
+
+
 
     
