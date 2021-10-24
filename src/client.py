bd04b853ebf8fc8f66541357d5cba95e67ca75e7
Chris Hulme <chris_hulme99@outlook.com>
2021-10-24 17:42:12 +0100

    Added comments to client-server

--- src/client.py
+++ src/client.py
@@ -7,7 +7,12 @@ import json
 
 
 class Client(object):
+
     def __init__(self, path: Path):
+        """ Constructor for client.
+        Args:
+            path (Path): Path of source directory to synchronise with server.
+        """
         self.source_path = path
         self.last_poll_time = datetime.fromtimestamp(
             100000)  # init to 1970-01-02
@@ -16,6 +21,12 @@ class Client(object):
         self.seen_files = []
 
     def run(self):
+        """ Client entrypoint
+
+        Created connection to server, and then:
+            1. Recursively scans the source directory for new, copied, modified, and deleted files.
+            2. Sends the original file path and encoded data to the server.
+        """
         self.socket.connect((socket.gethostname(), 12345))
 
         while self.running:
@@ -27,6 +38,14 @@ class Client(object):
             time.sleep(0.5)
 
     def get_files_in_dir(self, path: Path) -> list:
+        """ Recursively finds all the files in the target directory.
+
+        Args:
+            path (Path): Path of target directory to search.
+
+        Returns:
+            list: File paths found in target directory.
+        """
         files = []
         for file in Path(path).iterdir():
             if file.is_dir():
@@ -36,6 +55,16 @@ class Client(object):
         return files
 
     def get_modified_files(self, files: list) -> list:
+        """ Selects files that have been copied, modified, and deleted files.
+
+            This is done in a single function/loop for efficiency.
+
+        Args:
+            files (list): files to determine the status of.
+
+        Returns:
+            list: File paths of modified files.
+        """
         modified_files = []
         for file in files:
             if self.is_unseen_file(file) or self.has_file_changed(file):
@@ -43,13 +72,41 @@ class Client(object):
                 self.seen_files.append(file)
         return modified_files
 
-    def has_file_changed(self, src: Path) -> bool:
-        return src.stat().st_mtime > self.last_poll_time.timestamp()
+    def has_file_changed(self, file: Path) -> bool:
+        """ Determines if a file has been modified.
+
+            This is done by inspecting the file's modification timestamp metadata.
+            If the file has been updated after the last poll, the file has been modified.
+
+        Args:
+            files (Path): file to check if it has been modified.
+
+        Returns:
+            bool: Whether or not the file has been modified.
+        """
+        return file.stat().st_mtime > self.last_poll_time.timestamp()
 
     def is_unseen_file(self, path: Path) -> bool:
+        """ Determines if a file is new to the client.
+
+        Args:
+            files (Path): file to check.
+
+        Returns:
+            bool: Whether or not the file is new.
+        """
         return (path not in self.seen_files)
 
-    def send(self, files: list) -> bool:
+    def send(self, files: list):
+        """ Sends each modified file to the server.
+
+            Data in wrapped in a JSON format, containing the original file path and ISO8859-1 encoded data.
+            The use of ISO8859-1 encoding is because I've found it to be the most compatible with different
+            file types.
+
+        Args:
+            files (list): Files to send.
+        """
         for file in files:
             time.sleep(0.1)
             print(f'Client sending \'{file.name}\'')
@@ -58,5 +115,7 @@ class Client(object):
             self.socket.send(json_representation.encode())
 
     def terminate(self):
+        """ Called to safely stop the client.
+        """
         self.running = False
         self.socket.close()
--- src/server.py
+++ src/server.py
@@ -6,12 +6,22 @@ import json
 
 class Server(object):
     def __init__(self, path: Path):
+        """ Constructor for server.
+        Args:
+            path (Path): Path of source directory to synchronise with server.
+        """
         self.destination_path = path
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(('', 12345))
         self.running = True
 
     def run(self):
+        """ Server entrypoint
+
+        Establishes connection to client, and then receives JSON messages containing
+        the original file path and the file's encoded data. These fields are then used
+        to save the file in the appropriate location within the destination directory.
+        """
         self.socket.listen()
         conn, addr = self.socket.accept()
         print('Client-server connection established on', addr)
@@ -25,14 +35,24 @@ class Server(object):
         print("Client-server connection closed.")
 
     def save(self, file: dict):
+        """ Saves received files to the destination directory.
+
+        Args:
+            file (dict): Original file path and encoded data to save.
+        """
         dst_path = self.convert_src_to_dst_path(Path(file["path"]))
         print(f'Server saving {dst_path.name}')
         dst_path.parent.mkdir(parents=True, exist_ok=True)
         dst_path.write_bytes(bytes(file["data"], 'ISO8859-1'))
 
     def convert_src_to_dst_path(self, path: Path) -> Path:
+        """ Converts the received original path of a file, to the equivalent location
+            in the destination folder.
+        """
         return Path(str(path).replace("src", "dst"))
 
     def terminate(self):
+        """ Called to safely stop the server.
+        """
         self.running = False
         self.socket.close()
